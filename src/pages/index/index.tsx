ee5c58005ba65cca96b8402d531a22ee7c0f419c
Arterning <ning.huang.ridson@gmail.com>
2024-01-04 10:58:14 +0800

    init

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,27 @@
+import { View, Text, Button } from '@tarojs/components'
+import { useLoad } from '@tarojs/taro'
+import './index.scss'
+import { useState } from 'react'
+
+export default function Index() {
+
+  const [msg, setMsg] = useState('Welcom to taro')
+
+  useLoad(() => {
+    console.log('Page loaded.')
+  })
+
+  const onClick = () => {
+
+    setMsg(pre => pre + "!")
+  }
+
+  return (
+    <View className='index'>
+      <Text>Hello world!</Text>
+      <Button onClick={onClick}>Click me</Button>
+      <Text>You Are Good Person</Text>
+      <View>{msg}</View>
+    </View>
+  )
+}
